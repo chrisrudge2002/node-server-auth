3567536a8b89ffa5d0ba72886658228e5dc302b0
Chris Rudge <chrisrudge2002@gmail.com>
2017-02-19 11:35:44 +0000

    Enable CORS

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 // Entry point of the node application
 const bodyParser = require('body-parser');
+const cors = require('cors');
 const express = require('express');
 const http = require('http');
 const mongoose = require('mongoose');
@@ -13,6 +14,7 @@ mongoose.connect('mongodb://localhost:auth/auth');
 
 // Application Setup
 app.use(morgan('combined'));
+app.use(cors());
 app.use(bodyParser.json({ type: '*/*' }));
 router(app);
 
